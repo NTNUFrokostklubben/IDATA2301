a5c5750581844bb034fbe406568e45b132b00d2b
matrHS <matthew.r.hunt@gmail.com>
2025-01-28 12:36:52 +0100

    continued work on card component

--- src/component/coursecard.jsx
+++ src/component/coursecard.jsx
@@ -1,5 +1,19 @@
 export default function CourseCard() {
     return (
-        <p>This is a card</p>
+        <article className={"coursecard"}>
+            <a href={"#"}>
+                <img className={"cardImage"} src={"logo192.png"}
+                     alt={"logo"}
+                     width={"100px"}
+                     height={"100px"}/>
+                <section className={"cardContent"}>
+                    <h2 className={"cardTitle"}>Sed ut perspiciatis unde omnis iste natus
+                        error sit voluptatem accusantium</h2>
+                    <p className={"cardDesc"}>qui dolorem ipsum quia dolor</p>
+                    <p className={"cardPrice"}><b>14 000 NOK</b></p>
+                </section>
+            </a>
+        </article>
+
     )
 }
--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,28 +1,39 @@
+import CourseCard from "../component/coursecard";
+
 export default function Index() {
     return (
-        <>
-            <section id={"hero"}>
+        <div className={"App"}>
+            <section id={"hero"} className={"Hero"}>
                 <div>
                     <p>
                         Learniverse offers courses that give you the complete competence you need to succeed in the
                         workplace and beyond
                     </p>
-                    <button>
+                    <button className={"CTAbutton"}>
                         Try for free
                     </button>
                 </div>
             </section>
-            <section id={"courseinfo"}>
-                <div>
-                    <h1>
-                        Lorem ipsum dolor sit amet
-                    </h1>
-                    <p>
-                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
+            <section id={"courseinfo"} className={"CourseInfo"} >
+                <h1>
+                    Lorem ipsum dolor sit amet
+                </h1>
+                <p>
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <div className={"grid grid--4"}>
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
                 </div>
+
             </section>
-        </>
+        </div>
 
 
     )
