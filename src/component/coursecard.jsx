5f5b266a1e8679e021500bd0706b5a615a1f64b5
matrHS <matthew.r.hunt@gmail.com>
2025-01-28 10:47:00 +0100

    started work on mainpage basic HTML

---
+++ src/component/coursecard.jsx
@@ -0,0 +1,5 @@
+export default function CourseCard() {
+    return (
+        <p>This is a card</p>
+    )
+}
---
+++ src/page/index.jsx
@@ -0,0 +1,29 @@
+export default function Index() {
+    return (
+        <>
+            <section id={"hero"}>
+                <div>
+                    <p>
+                        Learniverse offers courses that give you the complete competence you need to succeed in the
+                        workplace and beyond
+                    </p>
+                    <button>
+                        Try for free
+                    </button>
+                </div>
+            </section>
+            <section id={"courseinfo"}>
+                <div>
+                    <h1>
+                        Lorem ipsum dolor sit amet
+                    </h1>
+                    <p>
+                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </div>
+            </section>
+        </>
+
+
+    )
+} 
