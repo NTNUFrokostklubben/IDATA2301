be9c3763bb486ced289805a0be183ba51a02ec26
matrHS <matthew.r.hunt@gmail.com>
2025-01-28 13:38:57 +0100

    added sample images, will be removed in the future. continued work on card component

--- src/component/coursecard.jsx
+++ src/component/coursecard.jsx
@@ -1,9 +1,19 @@
-export default function CourseCard() {
+export default function CourseCard(props) {
+    
+    class image {
+        constructor(name, path) {
+            this.name = name;
+            this.path = path;
+        }
+    }
+    
+    const cardImg =  new image(props.name, props.path)
+    
     return (
         <article className={"coursecard"}>
             <a href={"#"}>
-                <img className={"cardImage"} src={"logo192.png"}
-                     alt={"logo"}
+                <img className={"cardImage"} src={cardImg.path}
+                     alt={cardImg.name}
                      width={"100px"}
                      height={"100px"}/>
                 <section className={"cardContent"}>
--- src/page/index.jsx
+++ src/page/index.jsx
@@ -22,14 +22,16 @@ export default function Index() {
                     consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
                 <div className={"grid grid--4"}>
-                    <CourseCard/>
-                    <CourseCard/>
-                    <CourseCard/>
-                    <CourseCard/>
-                    <CourseCard/>
-                    <CourseCard/>
-                    <CourseCard/>
-                    <CourseCard/>
+                    <CourseCard name={"AWS"} path={"courses/AWS.png"}/>
+                    <CourseCard name={"Machine Learning"} path={"courses/machine_learning.jpg"}/>
+                    <CourseCard name={"Azure"} path={"courses/azure.png"}/>
+                    <CourseCard name={"Databricks"} path={"courses/Databricks_Logo.png"}/>
+                    <CourseCard name={"dotnet"} path={"courses/dotnet.jpg"}/>
+                    <CourseCard name={"SQL"} path={"courses/SQL.jpg"}/>
+                    <CourseCard name={"Java"} path={"courses/Java.png"}/>
+                    <CourseCard name={"Image Recognition"} path={"courses/ImageRecognition.jpg"}/>
+                    <CourseCard name={"SEO"} path={"courses/SEO.png"}/>
+                    
                 </div>
 
             </section>
