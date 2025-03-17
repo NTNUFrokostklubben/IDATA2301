f72022605420467cd10f7a4312ee1ce9953a78f6
matrHS <me@mhunt.no>
2025-03-17 11:45:01 +0100

    added an overview of all courses with buttons for edit/delete. Need to add logic to buttons as well as query to sort courses by provider

--- src/component/card/cardHorizontal.jsx
+++ src/component/card/cardHorizontal.jsx
@@ -1,62 +1,43 @@
-import "./card.css"
-import {useState} from "react";
+import "./cardHorizontal.css"
+import {useEffect, useState} from "react";
 
-export default function CardHorizontal(courseid) {
+export default function CardHorizontal(course) {
 
 
-    class course {
-        constructor(id, category, closest_course, credits, description, diff_level, hours_week, img_link, related_cert, title) {
-            this.id = id;
-            this.category = category;
-            this.closest_course = closest_course;
-            this.credits = credits;
-            this.description = description;
-            this.diff_level = diff_level;
-            this.hours_week = hours_week;
-            this.img_link = img_link;
-            this.related_cert = related_cert;
-            this.title = title;
+    if (!course) {
+        return <div>Post data is missing.</div>;
+    }
+
+    function getDifficulty (diff_level) {
+        switch (diff_level) {
+            case 0:
+                return "Beginner";
+            case 1:
+                return "Intermediate";
+            case 2:
+                return "Expert";
+            default:
+                return "Unknown";
         }
     }
 
-    async function getCourse(id) {
-        const response = await fetch(`http://localhost:8080/api/course/${id}`);
-        const data = await response.json();
-        console.log(data);
-        return data;
-    }
+    console.log(course);
 
-    function getDifficulty(diff) {
-        if (diff === 0) {
-            return "Beginner";
-        } else if (diff === 1) {
-            return "Intermediate";
-        } else {
-            return "Expert";
-        }
-    }
 
-    const courseData = getCourse(courseid.courseid);
-    const courseCard = new course(courseData.id, courseData.category, courseData.closest_course, courseData.credits, courseData.description, courseData.diff_level, courseData.hours_week, courseData.img_link, courseData.related_cert, courseData.title);
-
-    console.log(courseCard);
 
     return (
-        <section className="card">
-            <img className="course-img" src={courseCard.img_link} alt=""/>
-            <b>{courseCard.title}</b>
-            <p className="card-desc">{courseCard.description}</p>
-            <p className="infotext">{courseCard.hours_week} hours per week
-                • {getDifficulty(courseCard.diff_level)} • {courseCard.credits} ECTS credits</p>
-            <div className="card-footer">
-                <div className="rating">
-
-                    <img width="24" src="/icons/star-sharp.svg" alt=""/>
-                    <b>3.4</b>
-                    (152)
-
-                </div>
-                <p id="price2">10000 NOK</p>
+        <section className="cardAdmin" id={"course" + course.id}>
+            {/*<img className="course-img" src={course.imgLink} alt=""/>*/}
+            <img className="course-img" src={"https://picsum.photos/200/200?random=2"} alt=""/>
+            <p className={"card-title"}><b>{course.title}</b></p>
+            <p className="card-desc">{course.description}</p>
+            <p className="infotext">{course.hoursWeek} hours per week</p>
+            <p className={"difficulty"}>{getDifficulty(course.diffLevel)}</p>
+            <p className={"credits"}>{course.credits}ECTS credits</p>
+            <p className="price">10000 NOK</p>
+            <div className={"button-group"}>
+                <button className={"delete-button"} id={"delete" + course.id}>Delete</button>
+                <button className={"edit-button"} id={"edit" + course.id}>Edit</button>
             </div>
         </section>
     )
--- src/page/admin/course/courseIndex.jsx
+++ src/page/admin/course/courseIndex.jsx
@@ -1,9 +1,54 @@
 import "./courseIndex.css";
 import CardHorizontal from "../../../component/card/cardHorizontal";
+import {useEffect, useState} from "react";
+
+class courseEntity {
+    constructor(id, category, closestCourse, credits, description, diffLevel, hoursWeek, imgLink, relatedCert, title) {
+        this.id = id;
+        this.category = category;
+        this.closestCourse = closestCourse;
+        this.credits = credits;
+        this.description = description;
+        this.diffLevel = diffLevel;
+        this.hoursWeek = hoursWeek;
+        this.imgLink = imgLink;
+        this.relatedCert = relatedCert;
+        this.title = title;
+    }
+}
 
 export default function CourseIndex() {
 
+
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/courses")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                const courses = data.map((course) => new courseEntity(course.id, course.category, course.closestCourse, course.credits, course.description, course.diffLevel, course.hoursWeek, course.imgLink, course.relatedCert, course.title));
+                setCourses(courses);
+                setLoading(false)
+                console.log(courses)
+            })
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+
     return (
-        <CardHorizontal courseid={2}/>
+        <div>
+            <h1>Courses</h1>
+            <div>
+                {courses.map((course) => <CardHorizontal key={course.id} {...course}/>)}
+            </div>
+        </div>
     )
+
+
 }
