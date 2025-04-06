e5dd8e46925d6a7d21007fc08a856000f147c5ab
Signe BE <signebek@stud.ntnu.no>
2025-04-06 11:45:19 +0200

    The desc does no longer overflow the course cards.

--- src/component/card/CourseCard.jsx
+++ src/component/card/CourseCard.jsx
@@ -2,12 +2,12 @@ import "./courseCard.css"
 
 export default function CourseCard(course) {
     // TODO remove the temp value for the image link and price
-    var courceImg = "https://picsum.photos/200/200?random="+course.id;
+    var courseImg = "https://picsum.photos/200/200?random="+course.id;
     return (
         <section className="index-card" id={course.id} >
             <div className="index-course-card">
-                <img className="index-course-img" src={courceImg} alt="Course Card"/>
-                <h5>{course.title}</h5>
+                <img className="index-course-img" src={courseImg} alt="Course Card"/>
+                <h6>{course.title}</h6>
                 <p className="index-card-desc"> {course.description}</p>
                 <p>100 NOK -temp</p>
             </div>
