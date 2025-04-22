2dfd13a5979b273ff54764995113bff914094cbe
matrHS <matthew.r.hunt@gmail.com>
2025-04-22 09:28:38 +0200

    continued styling datepicker. implemented fields for offerablecourse

--- src/page/admin/course/add/offerableCourseAdd.jsx
+++ src/page/admin/course/add/offerableCourseAdd.jsx
@@ -11,6 +11,8 @@ export default function OfferableCourseAdd() {
 
     const [loading, setLoading] = useState(true);
 
+    const [startDate, setStartDate] = useState(new Date());
+
     useEffect(() => {
         fetch("http://localhost:8080/api/courses")
             .then((response) => response.json())
@@ -39,6 +41,8 @@ export default function OfferableCourseAdd() {
 
     registerLocale("nb", nb)
 
+
+
     return (
         <div className="offerableCourse-page">
             <h1>Add offerable course</h1>
@@ -61,7 +65,30 @@ export default function OfferableCourseAdd() {
                         </div>
                     </div>
 
-                    <DatePicker selected={new Date()} dateFormat={"dd-MM-YYYY"} locale={"nb"} showIcon  />
+                    <div className={"input-wrapper"}>
+                        <label htmlFor={"date"}>Start date</label>
+                        <DatePicker id={"date"} name={"date"} onChange={(date) => setStartDate(date)}
+                                    selected={startDate} dateFormat={"dd-MM-YYYY"} locale={"nb"}
+                                    icon={<img src={"/icons/calendar-clear-sharp.svg"}/>} showIcon/>
+                    </div>
+
+                    <div className={"group-2"}>
+                        <div className={"input-wrapper"}>
+                            <label htmlFor={"price"}>Price of course</label>
+                            <input min={0} type="number" id={"price"} name={"price"} required/>
+                        </div>
+                        <div className={"input-wrapper"}>
+                            <label htmlFor={"discount"}>Discount in percent</label>
+                            <input min={0} max={100} type="number" id={"discount"} name={"discount"} required/>
+                        </div>
+                    </div>
+
+                    <div className={"input-wrapper"}>
+                        <label htmlFor={"visibility"}>Visibility</label>
+                        <input type="checkbox" id={"visibility"} name={"visibility"}/>
+                    </div>
+
+                    <button type="submit" className={"button cta-button"}>Add Course</button>
 
                 </section>
 
