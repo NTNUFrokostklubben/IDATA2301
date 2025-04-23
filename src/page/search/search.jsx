a8174278b456a648f441daec29af5f8ebc18b10c
matrHS <matthew.r.hunt@gmail.com>
2025-04-23 09:58:28 +0200

    started work on search filters to backend, readied datepicker

--- src/page/search/search.jsx
+++ src/page/search/search.jsx
@@ -3,13 +3,17 @@ import Card from "../../component/card/card";
 import {useEffect, useState} from "react";
 import Collapsable from "../../component/Collapsable/collapsable";
 import {courseEntity} from "../../utils/Classes/commonClasses";
-
+import DatePicker from "react-datepicker";
 
 
 export default function Search() {
 
     const [courses, setCourses] = useState([]);
     const [loading, setLoading] = useState(true);
+
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -43,9 +47,10 @@ export default function Search() {
                     <Collapsable title={"Difficulty level"}>
                         <section id="difficulty">
 
-                            <label><input className="" type="checkbox" name={"diff-0"}/> Beginner</label>
-                            <label><input className="" type="checkbox" name={"diff-1"}/> Intermediate</label>
-                            <label><input className="" type="checkbox" name={"diff-2"}/> Expert</label>
+                            <label><input className="" type="checkbox" name={"diff-1"} value={true}/> Beginner</label>
+                            <label><input className="" type="checkbox" name={"diff-2"}
+                                          value={true}/> Intermediate</label>
+                            <label><input className="" type="checkbox" name={"diff-3"} value={true}/> Expert</label>
                         </section>
                     </Collapsable>
                     <Collapsable title={"Course size"}>
@@ -54,13 +59,13 @@ export default function Search() {
                             <div className="search-input-field-container">
                                 <div className="input-wrapper">
                                     <label htmlFor="min-size">Min</label>
-                                    <input className="" id="min-size" placeholder="Min" name={"min-credits"}
+                                    <input className="" id="min-size" placeholder="min" name={"min-credits"}
                                            type="number"/>
                                 </div>
-                                <span> - </span>
+                                <p> - </p>
                                 <div className="input-wrapper">
                                     <label htmlFor="max-size">Max</label>
-                                    <input className="" id="max-size" placeholder="Max" name={"max-credits"}
+                                    <input className="" id="max-size" placeholder="max" name={"max-credits"}
                                            type="number"/>
                                 </div>
                             </div>
@@ -85,14 +90,14 @@ export default function Search() {
                             <div className="search-input-field-container">
                                 <div className="input-wrapper">
                                     <label htmlFor="min-rating">Min rating</label>
-                                    <input id="min-rating" className="" placeholder="Min" name={"min-rating"}
+                                    <input id="min-rating" className="" placeholder="min" name={"min-rating"}
                                            type="number"/>
                                 </div>
-                                <span> - </span>
+                                <p> - </p>
 
                                 <div className="input-wrapper">
                                     <label htmlFor="max-rating">Max rating</label>
-                                    <input id="max-rating" className="" placeholder="Max" name={"max-rating"}
+                                    <input id="max-rating" className="" placeholder="max" name={"max-rating"}
                                            type="number"/>
                                 </div>
                             </div>
@@ -103,13 +108,13 @@ export default function Search() {
                             <div className="search-input-field-container">
                                 <div className="input-wrapper">
                                     <label htmlFor="min-price">Min price</label>
-                                    <input id="min-price" className="" placeholder="Min" name={"min-price"}
+                                    <input id="min-price" className="" placeholder="min" name={"min-price"}
                                            type="number"/>
                                 </div>
-                                <span> - </span>
+                                <p> - </p>
                                 <div className="input-wrapper">
                                     <label htmlFor="max-price">Max price</label>
-                                    <input id="max-price" className="" placeholder="Max price" name={"max-price"}
+                                    <input id="max-price" className="" placeholder="max" name={"max-price"}
                                            type="number"/>
                                 </div>
                             </div>
@@ -120,10 +125,17 @@ export default function Search() {
                         <section id={"dates"}>
                             <div className="search-input-field-container">
                                 <div className="input-wrapper"><label htmlFor="from-date">From</label>
-                                    <input id="from-date" name={"from-date"} type="date"/></div>
-                                <span> - </span>
+                                    <DatePicker id={"from-date"} name={"from-date"} selected={startDate}
+                                                onChange={(date) => setStartDate(date)}
+                                                selectsStart={true} startDate={startDate} endDate={endDate}
+                                                icon={<img src={"/icons/calendar-clear-sharp.svg"}/>} showIcon/></div>
+
+                                <p> - </p>
                                 <div className="input-wrapper"><label htmlFor="to-date">To</label>
-                                    <input id="to-date" name={"to-date"} type="date"/></div>
+                                    <DatePicker id="to-date" name={"to-date"} selected={endDate}
+                                                onChange={(date) => setEndDate(date)} selectsEnd={true}
+                                                startDate={startDate} endDate={endDate} minDate={startDate}
+                                                icon={<img src={"/icons/calendar-clear-sharp.svg"}/>} showIcon/></div>
                             </div>
                         </section>
                     </Collapsable>
@@ -131,11 +143,11 @@ export default function Search() {
                 </form>
             </div>
 
+
             <div className="search-results">
                 <section id="resultsinfo">
                     <h2>3 results for "Lorem Ipsum"</h2>
                     <div className="input-wrapper">
-                        <label htmlFor="filter-ropdown">Sort by</label>
                         <select className="filter-dropdown" id="filter-ropdown" type="dropdown">
                             <option>Most Relevant</option>
                             <option>Price Ascending</option>
