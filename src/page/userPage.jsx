73391ba9558fb663213206a6ab1cfeb4fd88bb72
augoks <augustdo@stud.ntnu.no>
2025-04-29 10:17:13 +0200

    Favorties now uses data from backend, user page is basically finished for now

--- src/page/userPage.jsx
+++ src/page/userPage.jsx
@@ -4,18 +4,21 @@ import CardHorizontal from "../component/card/cardHorizontal";
 import Rating from "../component/Rating/rating";
 import {AsyncApiRequest} from "../utils/requests";
 import {Link, useParams} from "react-router-dom";
+import FavoriteCard from "../component/favoriteCard/favoriteCard";
 
 export default function UserPage (){
-    const [data, setData] = useState([]);
+    const [courses, setCourses] = useState([]);
     const [user, setUser] = useState([]);
     const [loading, setLoading] = useState(true);
     const [ratings, setRatings] = useState([]);
+    const [favorites, setFavorites] = useState([]);
     const {id} = useParams();
 
     useEffect( () => {
         const fetchData = async () =>{
             try {
-                await Promise.any([handlePageLoad()])
+                await Promise.all([handleUserData(), handleCourseData(), handleFavoritesData()])
+                setLoading(false)
             }catch (e){console.log(e)}
         }
         fetchData()
@@ -23,20 +26,30 @@ export default function UserPage (){
     );
 
 
-    async function handlePageLoad(){
-        try{
+    async function handleUserData() {
+        try {
             const userData = await AsyncApiRequest("GET", `/user/${id}`, null)
                 .then(response => response.json())
             setUser(userData)
-            console.log(userData)
-            const ratingData = await AsyncApiRequest("GET", `/userCourses/${id}`, null)
-                .then(response => response.json())
-            setData(ratingData)
-            setRatings(ratingData.filter(item => item.rating > 0));
-            setLoading(false)
         }catch (e){console.error(e)}
     }
+    async function handleCourseData() {
+        try {
+            const courseData = await AsyncApiRequest("GET", `/userCourses/${id}`, null)
+                .then(response => response.json())
+            setCourses(courseData)
+            setRatings(courseData.filter(item => item.rating > 0));
 
+        }catch (e){console.error(e)}
+    }
+    async function handleFavoritesData() {
+        try {
+            const favoritesData = await AsyncApiRequest("GET", `/userFavorites/${id}`, null)
+                .then(response => response.json())
+            setFavorites(favoritesData)
+            console.log(favoritesData)
+        }catch (e){console.error(e)}
+    }
 
     if (loading ){
         return (<h5>loading...</h5>)
@@ -62,7 +75,7 @@ export default function UserPage (){
                 <section id="user-courses">
                     <h5 id="previous-courses-heading">Previous courses</h5>
                     <ul>
-                        {data.map(item => (
+                        {courses.map(item => (
                             <li className="user-course-item" key={item.id}> <Link to={`/course/${item.course.id}`}>{item.course.title}</Link></li>
                             ))}
                     </ul>
@@ -78,41 +91,7 @@ export default function UserPage (){
                 <section id="users-favorites">
                     <h5 id={"favorites-heading"}>Favorites</h5>
 
-                    <div className="one-favorite">
-                        <h2 className="favorite-course-title">Introduction to SQL Essentials</h2>
-                        <div className="favorite-image-and-text">
-                            <img className="favorite-course-image" src="https://picsum.photos/150/75" alt="user"/>
-                            <p className="favorite-course-text">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore
-                                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                laboris nisi
-                                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                velit esse
-                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                                sunt in culpa
-                                qui officia deserunt mollit anim id est laborum.
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="one-favorite">
-                        <h2 className="favorite-course-title">Introduction to SQL Essentials</h2>
-                        <div className="favorite-image-and-text">
-                            <img className="favorite-course-image" src="https://picsum.photos/150/75" alt="user"/>
-                            <p className="favorite-course-text">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore
-                                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                laboris nisi
-                                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                velit esse
-                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                                sunt in culpa
-                                qui officia deserunt mollit anim id est laborum.
-                            </p>
-                        </div>
-                    </div>
+                    {favorites.map(item => <FavoriteCard key={item.id} {...item.course}/>)}
 
                 </section>
             </section>
