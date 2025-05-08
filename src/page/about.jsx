19c265967c384b341caf65dce57fa954b9388472
SigneBEK <signebek@stud.ntnu.no>
2025-05-08 13:57:57 +0200

    Fixed matthew's comments.

--- src/page/about.jsx
+++ src/page/about.jsx
@@ -11,10 +11,10 @@ export default function About (){
                              alt="a worker working on a computer" id="about-hero-img"/>
                     </picture>
                     <div className={"about-hero-text-box"}>
-                        <h5 className="about-hero-text">
+                        <h6 className="about-hero-text">
                             Welcome to Learniverse Connect, your premier destination for unlocking a world of knowledge
                             and skills through our dynamic online course marketplace!
-                        </h5>
+                        </h6>
                     </div>
 
                 </div>
@@ -25,6 +25,7 @@ export default function About (){
                 <h2 className="blurbTitle">About us and our products</h2>
 
                 <div className="about-us-and-our-products-container">
+
                     <div className="about-us-and-our-products-grid">
 
                         <div className="about-section-text-box" id="about-section-text-box-1">
@@ -33,7 +34,10 @@ export default function About (){
                                 world of knowledge and skills through our dynamic online course marketplace.
                                 At Learniverse, we believe that learning knows no bounds, and our platform
                                 is designed to empower individuals like you to embark on a journey of lifelong
-                                learning. As a marketplace, we bring together a diverse array of courses
+                                learning.
+                            </p> &nbsp;
+                            <p className="about-section-text">
+                                As a marketplace, we bring together a diverse array of courses
                                 from passionate and expert third-party providers, ensuring that you have
                                 access to a comprehensive range of subjects and skills to fuel your personal
                                 and professional growth.
@@ -57,7 +61,10 @@ export default function About (){
                                 Whether you're a budding entrepreneur looking to master the intricacies
                                 of business strategy or someone seeking to delve into the realms of
                                 creative arts, Learniverse Connect is your trusted companion on the path
-                                to success. Join our vibrant community of learners, connect with top-notch
+                                to success.
+                            </p> &nbsp;
+                            <p className="about-section-text">
+                                Join our vibrant community of learners, connect with top-notch
                                 instructors, and explore a rich tapestry of knowledge that awaits you.
                                 At Learniverse, we envision a world where learning is not just a destination
                                 but a continuous, enriching journey, and we invite you to be a part of this
@@ -70,30 +77,34 @@ export default function About (){
             </section>
 
             <section className="about-our-mission">
-                <div className="about-our-mission-text-box">
-                    <p className="about-our-mission-text">
-                        While our courses predominantly take place in the virtual realm, we take pride in
-                        providing a unique blend of online learning and real-time engagement. Each course
-                        is facilitated by a dedicated physical instructor who not only guides you through
-                        the material but also ensures an interactive and dynamic learning experience.
-                        To further enrich your educational journey, we offer workshop sessions, adding
-                        a hands-on dimension to the online courses.
+
+                <div className="about-our-mission-content">
+                    <div className="about-our-mission-text-box">
+                        <p className="about-our-mission-text">
+                            While our courses predominantly take place in the virtual realm, we take pride in
+                            providing a unique blend of online learning and real-time engagement. Each course
+                            is facilitated by a dedicated physical instructor who not only guides you through
+                            the material but also ensures an interactive and dynamic learning experience.
+                            To further enrich your educational journey, we offer workshop sessions, adding
+                            a hands-on dimension to the online courses.
 
 
-                    </p> &nbsp;
-                    <p className="about-our-mission-text">
-                        While these courses are organized at specific dates to accommodate the workshop
-                        sessions, we understand the importance of flexibility. Rest assured, our
-                        commitment to your convenience is paramount, and the courses are strategically
-                        repeated several times a year, offering ample opportunities for you to participate
-                        and thrive in your learning pursuits.
-                    </p>
+                        </p> &nbsp;
+                        <p className="about-our-mission-text">
+                            While these courses are organized at specific dates to accommodate the workshop
+                            sessions, we understand the importance of flexibility. Rest assured, our
+                            commitment to your convenience is paramount, and the courses are strategically
+                            repeated several times a year, offering ample opportunities for you to participate
+                            and thrive in your learning pursuits.
+                        </p>
+                    </div>
+                    <div className="about-our-mission-img-box">
+                        <img className="about-our-mission-img" id="img-our-mission"
+                             src="/images/undraw_professor_d7zn.svg"
+                             alt="Professor teaching"/>
+                    </div>
                 </div>
-                <div className="about-our-mission-img-box">
-                    <img className="about-our-mission-img" id="img-our-mission"
-                         src="/images/undraw_professor_d7zn.svg"
-                         alt="Professor teaching"/>
-                </div>
+
             </section>
 
             <section id="about-feedback-section">
