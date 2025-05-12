93da39ea7917e9744d31757a07930f2be07a05a3
matrHS <matthew.r.hunt@gmail.com>
2025-05-12 13:37:52 +0200

    fixed linking to wrong page from reviews

--- src/component/Rating/review.jsx
+++ src/component/Rating/review.jsx
@@ -18,7 +18,7 @@ export default function Review ({rating: userCourse, title= false}){
 
     return (
         <div className="one-review">
-            {title &&  ( <Link to={`/course/${userCourse.id}` }>
+            {title &&  ( <Link to={`/course/${userCourse.course.id}` }>
                 <h3 className="review-title">{userCourse.course.title}</h3>
             </Link>)
             }
